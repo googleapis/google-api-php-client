87f0545d5f381b3ab9331db96e1882f2f5e94dfa
Silvano Luciani <silvano.luciani@gmail.com>
2013-12-20 20:24:14 -0800

    Updated Prediction.php

--- src/Google/Service/Prediction.php
+++ src/Google/Service/Prediction.php
@@ -66,138 +66,138 @@ class Google_Service_Prediction extends Google_Service
         $this->serviceName,
         'hostedmodels',
         array(
-    'methods' => array(
-          "predict" => array(
-            'path' => "{project}/hostedmodels/{hostedModelName}/predict",
-            'httpMethod' => "POST",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+          'methods' => array(
+            'predict' => array(
+              'path' => '{project}/hostedmodels/{hostedModelName}/predict',
+              'httpMethod' => 'POST',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
-              ),
-                "hostedModelName" => array(
-                  "location" => "path",
-                  "type" => "string",
+                ),
+                'hostedModelName' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
               ),
-              ),
-          ),
+            ),
+          )
         )
-    )
     );
     $this->trainedmodels = new Google_Service_Prediction_Trainedmodels_Resource(
         $this,
         $this->serviceName,
         'trainedmodels',
         array(
-    'methods' => array(
-          "analyze" => array(
-            'path' => "{project}/trainedmodels/{id}/analyze",
-            'httpMethod' => "GET",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+          'methods' => array(
+            'analyze' => array(
+              'path' => '{project}/trainedmodels/{id}/analyze',
+              'httpMethod' => 'GET',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
-              ),
-                "id" => array(
-                  "location" => "path",
-                  "type" => "string",
+                ),
+                'id' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
               ),
-              ),
-          ),"delete" => array(
-            'path' => "{project}/trainedmodels/{id}",
-            'httpMethod' => "DELETE",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+            ),'delete' => array(
+              'path' => '{project}/trainedmodels/{id}',
+              'httpMethod' => 'DELETE',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
-              ),
-                "id" => array(
-                  "location" => "path",
-                  "type" => "string",
+                ),
+                'id' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
               ),
-              ),
-          ),"get" => array(
-            'path' => "{project}/trainedmodels/{id}",
-            'httpMethod' => "GET",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+            ),'get' => array(
+              'path' => '{project}/trainedmodels/{id}',
+              'httpMethod' => 'GET',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
-              ),
-                "id" => array(
-                  "location" => "path",
-                  "type" => "string",
+                ),
+                'id' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
               ),
-              ),
-          ),"insert" => array(
-            'path' => "{project}/trainedmodels",
-            'httpMethod' => "POST",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+            ),'insert' => array(
+              'path' => '{project}/trainedmodels',
+              'httpMethod' => 'POST',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
               ),
-              ),
-          ),"list" => array(
-            'path' => "{project}/trainedmodels/list",
-            'httpMethod' => "GET",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+            ),'list' => array(
+              'path' => '{project}/trainedmodels/list',
+              'httpMethod' => 'GET',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
+                'pageToken' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
+                'maxResults' => array(
+                  'location' => 'query',
+                  'type' => 'integer',
+                ),
               ),
-                "pageToken" => array(
-                  "location" => "query",
-                  "type" => "string",
-              ),
-                "maxResults" => array(
-                  "location" => "query",
-                  "type" => "integer",
-              ),
-              ),
-          ),"predict" => array(
-            'path' => "{project}/trainedmodels/{id}/predict",
-            'httpMethod' => "POST",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+            ),'predict' => array(
+              'path' => '{project}/trainedmodels/{id}/predict',
+              'httpMethod' => 'POST',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
-              ),
-                "id" => array(
-                  "location" => "path",
-                  "type" => "string",
+                ),
+                'id' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
               ),
-              ),
-          ),"update" => array(
-            'path' => "{project}/trainedmodels/{id}",
-            'httpMethod' => "PUT",
-            'parameters' => array(
-                "project" => array(
-                  "location" => "path",
-                  "type" => "string",
+            ),'update' => array(
+              'path' => '{project}/trainedmodels/{id}',
+              'httpMethod' => 'PUT',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
-              ),
-                "id" => array(
-                  "location" => "path",
-                  "type" => "string",
+                ),
+                'id' => array(
+                  'location' => 'path',
+                  'type' => 'string',
                   'required' => true,
+                ),
               ),
-              ),
-          ),
+            ),
+          )
         )
-    )
     );
   }
 }
@@ -309,7 +309,7 @@ class Google_Service_Prediction_Trainedmodels_Resource extends Google_Service_Re
     return $this->call('insert', array($params), "Google_Service_Prediction_Insert2");
   }
   /**
-   * List available models. (trainedmodels.list)
+   * List available models. (trainedmodels.listTrainedmodels)
    *
    * @param string $project
    * The project associated with the model.
