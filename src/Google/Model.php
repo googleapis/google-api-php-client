2c575cb401b762a41738d9b66c2a2b7414c055b8
Ian Barber <ianbarber@google.com>
2014-01-10 15:00:27 +0000

    Style fix
    Move comment to satisfy the gods of identation

--- src/Google/Model.php
+++ src/Google/Model.php
@@ -84,9 +84,9 @@ class Google_Model implements ArrayAccess
         property_exists($this, $key)) {
           $this->$key = $val;
           unset($array[$key]);
-      // Check if property exists as camelCase, leave it in array as snake_case
-      // in case of backwards compatibility issues
       } elseif (property_exists($this, $camelKey = Google_Utils::camelCase($key))) {
+          // This checks if property exists as camelCase, leaving it in array as snake_case
+          // in case of backwards compatibility issues.
           $this->$camelKey = $val;
       }
     }
