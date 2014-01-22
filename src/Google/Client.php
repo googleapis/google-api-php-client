bc8c2e69fb0d87f0559502c4743e0c79ed458a55
Ian Barber <ianbarber@google.com>
2014-01-22 09:51:20 +0000

    Disable compress.zlib on GAE
    Currently unsupported.

--- src/Google/Client.php
+++ src/Google/Client.php
@@ -90,9 +90,11 @@ class Google_Client
     } else if ( !($config instanceof Google_Config)) {
       $config = new Google_Config();
 
-      // Automatically use Memcache if we're in AppEngine.
       if ($this->isAppEngine()) {
+        // Automatically use Memcache if we're in AppEngine.
         $config->setCacheClass('Google_Cache_Memcache');
+        // Automatically disable compress.zlib, as currently unsupported.
+        $config->setClassConfig('Google_Http_Request', 'disable_gzip', true);
       }
     }
 
