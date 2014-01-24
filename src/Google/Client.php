e46ae9f388684c85495c1ce7b12ba6c8862a181b
Ian Barber <ianbarber@google.com>
2014-01-24 11:34:41 +0000

    Bumping version to 1.0.2

--- src/Google/Client.php
+++ src/Google/Client.php
@@ -35,7 +35,7 @@ require_once 'Google/Service/Resource.php';
  */
 class Google_Client
 {
-  const LIBVER = "1.0.1-beta";
+  const LIBVER = "1.0.2-beta";
   const USER_AGENT_SUFFIX = "google-api-php-client/";
   const GZIP_UA = " (gzip)";
   /**
