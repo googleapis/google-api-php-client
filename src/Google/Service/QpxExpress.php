c1357f9ede685e4dc03cc28d19ee97f5ea81ad01
Silvano Luciani <silvano.luciani@gmail.com>
2014-02-21 21:27:11 -0800

    Updated QpxExpress.php

--- src/Google/Service/QpxExpress.php
+++ src/Google/Service/QpxExpress.php
@@ -24,7 +24,7 @@
  *
  * <p>
  * For more information about this service, see the API
- * <a href="" target="_blank">Documentation</a>
+ * <a href="http://developers.google.com/qpx-express" target="_blank">Documentation</a>
  * </p>
  *
  * @author Google, Inc.
