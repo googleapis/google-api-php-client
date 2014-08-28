99727b0129b23eaaf916d4a665e751d5732d563c
Silvano Luciani <silvano.luciani@gmail.com>
2014-08-28 16:03:50 -0700

    Updated Prediction.php

--- src/Google/Service/Prediction.php
+++ src/Google/Service/Prediction.php
@@ -363,6 +363,7 @@ class Google_Service_Prediction_Trainedmodels_Resource extends Google_Service_Re
 
 class Google_Service_Prediction_Analyze extends Google_Collection
 {
+  protected $collection_key = 'errors';
   protected $dataDescriptionType = 'Google_Service_Prediction_AnalyzeDataDescription';
   protected $dataDescriptionDataType = '';
   public $errors;
@@ -435,6 +436,7 @@ class Google_Service_Prediction_Analyze extends Google_Collection
 
 class Google_Service_Prediction_AnalyzeDataDescription extends Google_Collection
 {
+  protected $collection_key = 'features';
   protected $featuresType = 'Google_Service_Prediction_AnalyzeDataDescriptionFeatures';
   protected $featuresDataType = 'array';
   protected $outputFeatureType = 'Google_Service_Prediction_AnalyzeDataDescriptionOutputFeature';
@@ -514,6 +516,7 @@ class Google_Service_Prediction_AnalyzeDataDescriptionFeatures extends Google_Mo
 
 class Google_Service_Prediction_AnalyzeDataDescriptionFeaturesCategorical extends Google_Collection
 {
+  protected $collection_key = 'values';
   public $count;
   protected $valuesType = 'Google_Service_Prediction_AnalyzeDataDescriptionFeaturesCategoricalValues';
   protected $valuesDataType = 'array';
@@ -619,6 +622,7 @@ class Google_Service_Prediction_AnalyzeDataDescriptionFeaturesText extends Googl
 
 class Google_Service_Prediction_AnalyzeDataDescriptionOutputFeature extends Google_Collection
 {
+  protected $collection_key = 'text';
   protected $numericType = 'Google_Service_Prediction_AnalyzeDataDescriptionOutputFeatureNumeric';
   protected $numericDataType = '';
   protected $textType = 'Google_Service_Prediction_AnalyzeDataDescriptionOutputFeatureText';
@@ -784,6 +788,7 @@ class Google_Service_Prediction_Input extends Google_Model
 
 class Google_Service_Prediction_InputInput extends Google_Collection
 {
+  protected $collection_key = 'csvInstance';
   public $csvInstance;
 
   public function setCsvInstance($csvInstance)
@@ -799,6 +804,7 @@ class Google_Service_Prediction_InputInput extends Google_Collection
 
 class Google_Service_Prediction_Insert extends Google_Collection
 {
+  protected $collection_key = 'utility';
   public $id;
   public $modelType;
   public $sourceModel;
@@ -1088,6 +1094,7 @@ class Google_Service_Prediction_Insert2ModelInfo extends Google_Model
 
 class Google_Service_Prediction_InsertTrainingInstances extends Google_Collection
 {
+  protected $collection_key = 'csvInstance';
   public $csvInstance;
   public $output;
 
@@ -1119,6 +1126,7 @@ class Google_Service_Prediction_InsertUtility extends Google_Model
 
 class Google_Service_Prediction_Output extends Google_Collection
 {
+  protected $collection_key = 'outputMulti';
   public $id;
   public $kind;
   public $outputLabel;
@@ -1216,6 +1224,7 @@ class Google_Service_Prediction_OutputOutputMulti extends Google_Model
 
 class Google_Service_Prediction_PredictionList extends Google_Collection
 {
+  protected $collection_key = 'items';
   protected $itemsType = 'Google_Service_Prediction_Insert2';
   protected $itemsDataType = 'array';
   public $kind;
@@ -1265,6 +1274,7 @@ class Google_Service_Prediction_PredictionList extends Google_Collection
 
 class Google_Service_Prediction_Update extends Google_Collection
 {
+  protected $collection_key = 'csvInstance';
   public $csvInstance;
   public $output;
 
