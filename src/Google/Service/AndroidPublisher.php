948bb5717bb56f7dde9bff696587bc5d1fcc0eab
Silvano Luciani <silvano.luciani@gmail.com>
2014-08-28 16:03:14 -0700

    Updated AndroidPublisher.php

--- src/Google/Service/AndroidPublisher.php
+++ src/Google/Service/AndroidPublisher.php
@@ -2187,6 +2187,7 @@ class Google_Service_AndroidPublisher_ApkListing extends Google_Model
 
 class Google_Service_AndroidPublisher_ApkListingsListResponse extends Google_Collection
 {
+  protected $collection_key = 'listings';
   public $kind;
   protected $listingsType = 'Google_Service_AndroidPublisher_ApkListing';
   protected $listingsDataType = 'array';
@@ -2214,6 +2215,7 @@ class Google_Service_AndroidPublisher_ApkListingsListResponse extends Google_Col
 
 class Google_Service_AndroidPublisher_ApksListResponse extends Google_Collection
 {
+  protected $collection_key = 'apks';
   protected $apksType = 'Google_Service_AndroidPublisher_Apk';
   protected $apksDataType = 'array';
   public $kind;
@@ -2394,6 +2396,7 @@ class Google_Service_AndroidPublisher_Image extends Google_Model
 
 class Google_Service_AndroidPublisher_ImagesDeleteAllResponse extends Google_Collection
 {
+  protected $collection_key = 'deleted';
   protected $deletedType = 'Google_Service_AndroidPublisher_Image';
   protected $deletedDataType = 'array';
 
@@ -2410,6 +2413,7 @@ class Google_Service_AndroidPublisher_ImagesDeleteAllResponse extends Google_Col
 
 class Google_Service_AndroidPublisher_ImagesListResponse extends Google_Collection
 {
+  protected $collection_key = 'images';
   protected $imagesType = 'Google_Service_AndroidPublisher_Image';
   protected $imagesDataType = 'array';
 
@@ -2595,6 +2599,7 @@ class Google_Service_AndroidPublisher_InAppProductPrices extends Google_Model
 
 class Google_Service_AndroidPublisher_InappproductsBatchRequest extends Google_Collection
 {
+  protected $collection_key = 'entrys';
   protected $entrysType = 'Google_Service_AndroidPublisher_InappproductsBatchRequestEntry';
   protected $entrysDataType = 'array';
 
@@ -2661,6 +2666,7 @@ class Google_Service_AndroidPublisher_InappproductsBatchRequestEntry extends Goo
 
 class Google_Service_AndroidPublisher_InappproductsBatchResponse extends Google_Collection
 {
+  protected $collection_key = 'entrys';
   protected $entrysType = 'Google_Service_AndroidPublisher_InappproductsBatchResponseEntry';
   protected $entrysDataType = 'array';
   public $kind;
@@ -2759,6 +2765,7 @@ class Google_Service_AndroidPublisher_InappproductsInsertResponse extends Google
 
 class Google_Service_AndroidPublisher_InappproductsListResponse extends Google_Collection
 {
+  protected $collection_key = 'inappproduct';
   protected $inappproductType = 'Google_Service_AndroidPublisher_InAppProduct';
   protected $inappproductDataType = 'array';
   public $kind;
@@ -2901,6 +2908,7 @@ class Google_Service_AndroidPublisher_Listing extends Google_Model
 
 class Google_Service_AndroidPublisher_ListingsListResponse extends Google_Collection
 {
+  protected $collection_key = 'listings';
   public $kind;
   protected $listingsType = 'Google_Service_AndroidPublisher_Listing';
   protected $listingsDataType = 'array';
@@ -3098,6 +3106,7 @@ class Google_Service_AndroidPublisher_SubscriptionPurchase extends Google_Model
 
 class Google_Service_AndroidPublisher_Testers extends Google_Collection
 {
+  protected $collection_key = 'googlePlusCommunities';
   public $googleGroups;
   public $googlePlusCommunities;
 
@@ -3150,6 +3159,7 @@ class Google_Service_AndroidPublisher_TokenPagination extends Google_Model
 
 class Google_Service_AndroidPublisher_Track extends Google_Collection
 {
+  protected $collection_key = 'versionCodes';
   public $track;
   public $userFraction;
   public $versionCodes;
@@ -3187,6 +3197,7 @@ class Google_Service_AndroidPublisher_Track extends Google_Collection
 
 class Google_Service_AndroidPublisher_TracksListResponse extends Google_Collection
 {
+  protected $collection_key = 'tracks';
   public $kind;
   protected $tracksType = 'Google_Service_AndroidPublisher_Track';
   protected $tracksDataType = 'array';
