ce9255b68b33e8ec2216d82871958adc19d023bd
Silvano Luciani <silvano.luciani@gmail.com>
2014-11-08 02:00:24 -0800

    Updated Replicapoolupdater.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Replicapoolupdater.php
+++ src/Google/Service/Replicapoolupdater.php
@@ -24,7 +24,7 @@
  *
  * <p>
  * For more information about this service, see the API
- * <a href="" target="_blank">Documentation</a>
+ * <a href="https://developers.google.com/compute/docs/instance-groups/manager/v1beta2" target="_blank">Documentation</a>
  * </p>
  *
  * @author Google, Inc.
