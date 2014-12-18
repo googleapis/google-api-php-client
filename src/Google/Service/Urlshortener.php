cea3e8db54ee19676c32547ecff76ec1ba734f23
Silvano Luciani <silvano.luciani@gmail.com>
2014-12-18 05:43:52 -0500

    Updated Urlshortener.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Urlshortener.php
+++ src/Google/Service/Urlshortener.php
@@ -23,7 +23,7 @@
  *
  * <p>
  * For more information about this service, see the API
- * <a href="http://code.google.com/apis/urlshortener/v1/getting_started.html" target="_blank">Documentation</a>
+ * <a href="https://developers.google.com/url-shortener/v1/getting_started" target="_blank">Documentation</a>
  * </p>
  *
  * @author Google, Inc.
