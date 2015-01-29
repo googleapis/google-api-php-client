8c540a298b60ea9ef0000bce5f59abea0ec0aac0
Silvano Luciani <silvano.luciani@gmail.com>
2015-01-29 03:25:54 -0800

    Updated Replicapoolupdater.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Replicapoolupdater.php
+++ src/Google/Service/Replicapoolupdater.php
@@ -42,6 +42,7 @@ class Google_Service_Replicapoolupdater extends Google_Service
       "https://www.googleapis.com/auth/replicapool.readonly";
 
   public $rollingUpdates;
+  public $zoneOperations;
   
 
   /**
@@ -244,6 +245,36 @@ class Google_Service_Replicapoolupdater extends Google_Service
           )
         )
     );
+    $this->zoneOperations = new Google_Service_Replicapoolupdater_ZoneOperations_Resource(
+        $this,
+        $this->serviceName,
+        'zoneOperations',
+        array(
+          'methods' => array(
+            'get' => array(
+              'path' => '{project}/zones/{zone}/operations/{operation}',
+              'httpMethod' => 'GET',
+              'parameters' => array(
+                'project' => array(
+                  'location' => 'path',
+                  'type' => 'string',
+                  'required' => true,
+                ),
+                'zone' => array(
+                  'location' => 'path',
+                  'type' => 'string',
+                  'required' => true,
+                ),
+                'operation' => array(
+                  'location' => 'path',
+                  'type' => 'string',
+                  'required' => true,
+                ),
+              ),
+            ),
+          )
+        )
+    );
   }
 }
 
@@ -268,12 +299,13 @@ class Google_Service_Replicapoolupdater_RollingUpdates_Resource extends Google_S
    * resides.
    * @param string $rollingUpdate The name of the update.
    * @param array $optParams Optional parameters.
+   * @return Google_Service_Replicapoolupdater_Operation
    */
   public function cancel($project, $zone, $rollingUpdate, $optParams = array())
   {
     $params = array('project' => $project, 'zone' => $zone, 'rollingUpdate' => $rollingUpdate);
     $params = array_merge($params, $optParams);
-    return $this->call('cancel', array($params));
+    return $this->call('cancel', array($params), "Google_Service_Replicapoolupdater_Operation");
   }
 
   /**
@@ -301,13 +333,13 @@ class Google_Service_Replicapoolupdater_RollingUpdates_Resource extends Google_S
    * resides.
    * @param Google_RollingUpdate $postBody
    * @param array $optParams Optional parameters.
-   * @return Google_Service_Replicapoolupdater_InsertResponse
+   * @return Google_Service_Replicapoolupdater_Operation
    */
   public function insert($project, $zone, Google_Service_Replicapoolupdater_RollingUpdate $postBody, $optParams = array())
   {
     $params = array('project' => $project, 'zone' => $zone, 'postBody' => $postBody);
     $params = array_merge($params, $optParams);
-    return $this->call('insert', array($params), "Google_Service_Replicapoolupdater_InsertResponse");
+    return $this->call('insert', array($params), "Google_Service_Replicapoolupdater_Operation");
   }
 
   /**
@@ -325,8 +357,8 @@ class Google_Service_Replicapoolupdater_RollingUpdates_Resource extends Google_S
    * resources.
    * @opt_param string pageToken Optional. Tag returned by a previous list request
    * truncated by maxResults. Used to continue a previous list request.
-   * @opt_param string instanceGroupManager The name of the instance group
-   * manager.
+   * @opt_param string instanceGroupManager The name of the instance group manager
+   * used for filtering.
    * @return Google_Service_Replicapoolupdater_RollingUpdateList
    */
   public function listRollingUpdates($project, $zone, $optParams = array())
@@ -371,12 +403,13 @@ class Google_Service_Replicapoolupdater_RollingUpdates_Resource extends Google_S
    * resides.
    * @param string $rollingUpdate The name of the update.
    * @param array $optParams Optional parameters.
+   * @return Google_Service_Replicapoolupdater_Operation
    */
   public function pause($project, $zone, $rollingUpdate, $optParams = array())
   {
     $params = array('project' => $project, 'zone' => $zone, 'rollingUpdate' => $rollingUpdate);
     $params = array_merge($params, $optParams);
-    return $this->call('pause', array($params));
+    return $this->call('pause', array($params), "Google_Service_Replicapoolupdater_Operation");
   }
 
   /**
@@ -388,12 +421,13 @@ class Google_Service_Replicapoolupdater_RollingUpdates_Resource extends Google_S
    * resides.
    * @param string $rollingUpdate The name of the update.
    * @param array $optParams Optional parameters.
+   * @return Google_Service_Replicapoolupdater_Operation
    */
   public function resume($project, $zone, $rollingUpdate, $optParams = array())
   {
     $params = array('project' => $project, 'zone' => $zone, 'rollingUpdate' => $rollingUpdate);
     $params = array_merge($params, $optParams);
-    return $this->call('resume', array($params));
+    return $this->call('resume', array($params), "Google_Service_Replicapoolupdater_Operation");
   }
 
   /**
@@ -406,43 +440,66 @@ class Google_Service_Replicapoolupdater_RollingUpdates_Resource extends Google_S
    * resides.
    * @param string $rollingUpdate The name of the update.
    * @param array $optParams Optional parameters.
+   * @return Google_Service_Replicapoolupdater_Operation
    */
   public function rollback($project, $zone, $rollingUpdate, $optParams = array())
   {
     $params = array('project' => $project, 'zone' => $zone, 'rollingUpdate' => $rollingUpdate);
     $params = array_merge($params, $optParams);
-    return $this->call('rollback', array($params));
+    return $this->call('rollback', array($params), "Google_Service_Replicapoolupdater_Operation");
   }
 }
 
-
-
-
-class Google_Service_Replicapoolupdater_InsertResponse extends Google_Model
+/**
+ * The "zoneOperations" collection of methods.
+ * Typical usage is:
+ *  <code>
+ *   $replicapoolupdaterService = new Google_Service_Replicapoolupdater(...);
+ *   $zoneOperations = $replicapoolupdaterService->zoneOperations;
+ *  </code>
+ */
+class Google_Service_Replicapoolupdater_ZoneOperations_Resource extends Google_Service_Resource
 {
-  protected $internal_gapi_mappings = array(
-  );
-  public $rollingUpdate;
 
-
-  public function setRollingUpdate($rollingUpdate)
+  /**
+   * Retrieves the specified zone-specific operation resource.
+   * (zoneOperations.get)
+   *
+   * @param string $project Name of the project scoping this request.
+   * @param string $zone Name of the zone scoping this request.
+   * @param string $operation Name of the operation resource to return.
+   * @param array $optParams Optional parameters.
+   * @return Google_Service_Replicapoolupdater_Operation
+   */
+  public function get($project, $zone, $operation, $optParams = array())
   {
-    $this->rollingUpdate = $rollingUpdate;
-  }
-  public function getRollingUpdate()
-  {
-    return $this->rollingUpdate;
+    $params = array('project' => $project, 'zone' => $zone, 'operation' => $operation);
+    $params = array_merge($params, $optParams);
+    return $this->call('get', array($params), "Google_Service_Replicapoolupdater_Operation");
   }
 }
+
+
+
 
 class Google_Service_Replicapoolupdater_InstanceUpdate extends Google_Model
 {
   protected $internal_gapi_mappings = array(
   );
+  protected $errorType = 'Google_Service_Replicapoolupdater_InstanceUpdateError';
+  protected $errorDataType = '';
   public $instance;
   public $status;
 
 
+  public function setError(Google_Service_Replicapoolupdater_InstanceUpdateError $error)
+  {
+    $this->error = $error;
+  }
+  public function getError()
+  {
+    return $this->error;
+  }
   public function setInstance($instance)
   {
     $this->instance = $instance;
@@ -458,6 +515,60 @@ class Google_Service_Replicapoolupdater_InstanceUpdate extends Google_Model
   public function getStatus()
   {
     return $this->status;
+  }
+}
+
+class Google_Service_Replicapoolupdater_InstanceUpdateError extends Google_Collection
+{
+  protected $collection_key = 'errors';
+  protected $internal_gapi_mappings = array(
+  );
+  protected $errorsType = 'Google_Service_Replicapoolupdater_InstanceUpdateErrorErrors';
+  protected $errorsDataType = 'array';
+
+
+  public function setErrors($errors)
+  {
+    $this->errors = $errors;
+  }
+  public function getErrors()
+  {
+    return $this->errors;
+  }
+}
+
+class Google_Service_Replicapoolupdater_InstanceUpdateErrorErrors extends Google_Model
+{
+  protected $internal_gapi_mappings = array(
+  );
+  public $code;
+  public $location;
+  public $message;
+
+
+  public function setCode($code)
+  {
+    $this->code = $code;
+  }
+  public function getCode()
+  {
+    return $this->code;
+  }
+  public function setLocation($location)
+  {
+    $this->location = $location;
+  }
+  public function getLocation()
+  {
+    return $this->location;
+  }
+  public function setMessage($message)
+  {
+    $this->message = $message;
+  }
+  public function getMessage()
+  {
+    return $this->message;
   }
 }
 
@@ -507,13 +618,341 @@ class Google_Service_Replicapoolupdater_InstanceUpdateList extends Google_Collec
   }
 }
 
+class Google_Service_Replicapoolupdater_Operation extends Google_Collection
+{
+  protected $collection_key = 'warnings';
+  protected $internal_gapi_mappings = array(
+  );
+  public $clientOperationId;
+  public $creationTimestamp;
+  public $endTime;
+  protected $errorType = 'Google_Service_Replicapoolupdater_OperationError';
+  protected $errorDataType = '';
+  public $httpErrorMessage;
+  public $httpErrorStatusCode;
+  public $id;
+  public $insertTime;
+  public $kind;
+  public $name;
+  public $operationType;
+  public $progress;
+  public $region;
+  public $selfLink;
+  public $startTime;
+  public $status;
+  public $statusMessage;
+  public $targetId;
+  public $targetLink;
+  public $user;
+  protected $warningsType = 'Google_Service_Replicapoolupdater_OperationWarnings';
+  protected $warningsDataType = 'array';
+  public $zone;
+
+
+  public function setClientOperationId($clientOperationId)
+  {
+    $this->clientOperationId = $clientOperationId;
+  }
+  public function getClientOperationId()
+  {
+    return $this->clientOperationId;
+  }
+  public function setCreationTimestamp($creationTimestamp)
+  {
+    $this->creationTimestamp = $creationTimestamp;
+  }
+  public function getCreationTimestamp()
+  {
+    return $this->creationTimestamp;
+  }
+  public function setEndTime($endTime)
+  {
+    $this->endTime = $endTime;
+  }
+  public function getEndTime()
+  {
+    return $this->endTime;
+  }
+  public function setError(Google_Service_Replicapoolupdater_OperationError $error)
+  {
+    $this->error = $error;
+  }
+  public function getError()
+  {
+    return $this->error;
+  }
+  public function setHttpErrorMessage($httpErrorMessage)
+  {
+    $this->httpErrorMessage = $httpErrorMessage;
+  }
+  public function getHttpErrorMessage()
+  {
+    return $this->httpErrorMessage;
+  }
+  public function setHttpErrorStatusCode($httpErrorStatusCode)
+  {
+    $this->httpErrorStatusCode = $httpErrorStatusCode;
+  }
+  public function getHttpErrorStatusCode()
+  {
+    return $this->httpErrorStatusCode;
+  }
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+  public function getId()
+  {
+    return $this->id;
+  }
+  public function setInsertTime($insertTime)
+  {
+    $this->insertTime = $insertTime;
+  }
+  public function getInsertTime()
+  {
+    return $this->insertTime;
+  }
+  public function setKind($kind)
+  {
+    $this->kind = $kind;
+  }
+  public function getKind()
+  {
+    return $this->kind;
+  }
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+  public function getName()
+  {
+    return $this->name;
+  }
+  public function setOperationType($operationType)
+  {
+    $this->operationType = $operationType;
+  }
+  public function getOperationType()
+  {
+    return $this->operationType;
+  }
+  public function setProgress($progress)
+  {
+    $this->progress = $progress;
+  }
+  public function getProgress()
+  {
+    return $this->progress;
+  }
+  public function setRegion($region)
+  {
+    $this->region = $region;
+  }
+  public function getRegion()
+  {
+    return $this->region;
+  }
+  public function setSelfLink($selfLink)
+  {
+    $this->selfLink = $selfLink;
+  }
+  public function getSelfLink()
+  {
+    return $this->selfLink;
+  }
+  public function setStartTime($startTime)
+  {
+    $this->startTime = $startTime;
+  }
+  public function getStartTime()
+  {
+    return $this->startTime;
+  }
+  public function setStatus($status)
+  {
+    $this->status = $status;
+  }
+  public function getStatus()
+  {
+    return $this->status;
+  }
+  public function setStatusMessage($statusMessage)
+  {
+    $this->statusMessage = $statusMessage;
+  }
+  public function getStatusMessage()
+  {
+    return $this->statusMessage;
+  }
+  public function setTargetId($targetId)
+  {
+    $this->targetId = $targetId;
+  }
+  public function getTargetId()
+  {
+    return $this->targetId;
+  }
+  public function setTargetLink($targetLink)
+  {
+    $this->targetLink = $targetLink;
+  }
+  public function getTargetLink()
+  {
+    return $this->targetLink;
+  }
+  public function setUser($user)
+  {
+    $this->user = $user;
+  }
+  public function getUser()
+  {
+    return $this->user;
+  }
+  public function setWarnings($warnings)
+  {
+    $this->warnings = $warnings;
+  }
+  public function getWarnings()
+  {
+    return $this->warnings;
+  }
+  public function setZone($zone)
+  {
+    $this->zone = $zone;
+  }
+  public function getZone()
+  {
+    return $this->zone;
+  }
+}
+
+class Google_Service_Replicapoolupdater_OperationError extends Google_Collection
+{
+  protected $collection_key = 'errors';
+  protected $internal_gapi_mappings = array(
+  );
+  protected $errorsType = 'Google_Service_Replicapoolupdater_OperationErrorErrors';
+  protected $errorsDataType = 'array';
+
+
+  public function setErrors($errors)
+  {
+    $this->errors = $errors;
+  }
+  public function getErrors()
+  {
+    return $this->errors;
+  }
+}
+
+class Google_Service_Replicapoolupdater_OperationErrorErrors extends Google_Model
+{
+  protected $internal_gapi_mappings = array(
+  );
+  public $code;
+  public $location;
+  public $message;
+
+
+  public function setCode($code)
+  {
+    $this->code = $code;
+  }
+  public function getCode()
+  {
+    return $this->code;
+  }
+  public function setLocation($location)
+  {
+    $this->location = $location;
+  }
+  public function getLocation()
+  {
+    return $this->location;
+  }
+  public function setMessage($message)
+  {
+    $this->message = $message;
+  }
+  public function getMessage()
+  {
+    return $this->message;
+  }
+}
+
+class Google_Service_Replicapoolupdater_OperationWarnings extends Google_Collection
+{
+  protected $collection_key = 'data';
+  protected $internal_gapi_mappings = array(
+  );
+  public $code;
+  protected $dataType = 'Google_Service_Replicapoolupdater_OperationWarningsData';
+  protected $dataDataType = 'array';
+  public $message;
+
+
+  public function setCode($code)
+  {
+    $this->code = $code;
+  }
+  public function getCode()
+  {
+    return $this->code;
+  }
+  public function setData($data)
+  {
+    $this->data = $data;
+  }
+  public function getData()
+  {
+    return $this->data;
+  }
+  public function setMessage($message)
+  {
+    $this->message = $message;
+  }
+  public function getMessage()
+  {
+    return $this->message;
+  }
+}
+
+class Google_Service_Replicapoolupdater_OperationWarningsData extends Google_Model
+{
+  protected $internal_gapi_mappings = array(
+  );
+  public $key;
+  public $value;
+
+
+  public function setKey($key)
+  {
+    $this->key = $key;
+  }
+  public function getKey()
+  {
+    return $this->key;
+  }
+  public function setValue($value)
+  {
+    $this->value = $value;
+  }
+  public function getValue()
+  {
+    return $this->value;
+  }
+}
+
 class Google_Service_Replicapoolupdater_RollingUpdate extends Google_Model
 {
   protected $internal_gapi_mappings = array(
   );
   public $actionType;
   public $creationTimestamp;
+  public $description;
   public $id;
+  public $instanceGroup;
   public $instanceGroupManager;
   public $instanceTemplate;
   public $kind;
@@ -542,6 +981,14 @@ class Google_Service_Replicapoolupdater_RollingUpdate extends Google_Model
   {
     return $this->creationTimestamp;
   }
+  public function setDescription($description)
+  {
+    $this->description = $description;
+  }
+  public function getDescription()
+  {
+    return $this->description;
+  }
   public function setId($id)
   {
     $this->id = $id;
@@ -549,6 +996,14 @@ class Google_Service_Replicapoolupdater_RollingUpdate extends Google_Model
   public function getId()
   {
     return $this->id;
+  }
+  public function setInstanceGroup($instanceGroup)
+  {
+    $this->instanceGroup = $instanceGroup;
+  }
+  public function getInstanceGroup()
+  {
+    return $this->instanceGroup;
   }
   public function setInstanceGroupManager($instanceGroupManager)
   {
