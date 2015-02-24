89df9a568750bce1f30c1121aabcc1e8337332b1
Ian Barber <ianbarber@google.com>
2015-02-24 11:50:29 -0800

    Tidy up
    Add note about tests to README
    Fix compute engine autoload reference
    Bump client version

--- src/Google/Client.php
+++ src/Google/Client.php
@@ -26,7 +26,7 @@ require_once realpath(dirname(__FILE__) . '/autoload.php');
  */
 class Google_Client
 {
-  const LIBVER = "1.1.2";
+  const LIBVER = "1.1.3";
   const USER_AGENT_SUFFIX = "google-api-php-client/";
   /**
    * @var Google_Auth_Abstract $auth
