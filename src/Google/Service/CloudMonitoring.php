644c9b71ed5eb423a114d6776187d9c0cc025ab4
Silvano Luciani <silvano.luciani@gmail.com>
2015-02-26 03:53:02 -0800

    Updated CloudMonitoring.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/CloudMonitoring.php
+++ src/Google/Service/CloudMonitoring.php
@@ -23,7 +23,7 @@
  *
  * <p>
  * For more information about this service, see the API
- * <a href="https://developers.google.com/cloud/eap/cloud-monitoring/v2beta2/" target="_blank">Documentation</a>
+ * <a href="https://cloud.google.com/monitoring/v2beta2/" target="_blank">Documentation</a>
  * </p>
  *
  * @author Google, Inc.
