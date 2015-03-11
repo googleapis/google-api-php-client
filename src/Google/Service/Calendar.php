eb9c21866f364f813623269d449644663f7265c4
Silvano Luciani <silvano.luciani@gmail.com>
2015-03-11 05:05:05 -0700

    Updated Calendar.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Calendar.php
+++ src/Google/Service/Calendar.php
@@ -1212,8 +1212,8 @@ class Google_Service_Calendar_Calendars_Resource extends Google_Service_Resource
 {
 
   /**
-   * Clears a primary calendar. This operation deletes all data associated with
-   * the primary calendar of an account and cannot be undone. (calendars.clear)
+   * Clears a primary calendar. This operation deletes all events associated with
+   * the primary calendar of an account. (calendars.clear)
    *
    * @param string $calendarId Calendar identifier.
    * @param array $optParams Optional parameters.
@@ -1226,7 +1226,8 @@ class Google_Service_Calendar_Calendars_Resource extends Google_Service_Resource
   }
 
   /**
-   * Deletes a secondary calendar. (calendars.delete)
+   * Deletes a secondary calendar. Use calendars.clear for clearing all events on
+   * primary calendars. (calendars.delete)
    *
    * @param string $calendarId Calendar identifier.
    * @param array $optParams Optional parameters.
