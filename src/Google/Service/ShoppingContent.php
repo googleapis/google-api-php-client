788b4a417c9604030c68c49fa2d4d8c9fe77ebd3
Silvano Luciani <silvano.luciani@gmail.com>
2015-03-20 05:13:26 -0700

    Updated ShoppingContent.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/ShoppingContent.php
+++ src/Google/Service/ShoppingContent.php
@@ -4277,6 +4277,8 @@ class Google_Service_ShoppingContent_Product extends Google_Collection
   public $adwordsLabels;
   public $adwordsRedirect;
   public $ageGroup;
+  protected $aspectsType = 'Google_Service_ShoppingContent_ProductAspect';
+  protected $aspectsDataType = 'array';
   public $availability;
   public $availabilityDate;
   public $brand;
@@ -4404,6 +4406,14 @@ class Google_Service_ShoppingContent_Product extends Google_Collection
   public function getAgeGroup()
   {
     return $this->ageGroup;
+  }
+  public function setAspects($aspects)
+  {
+    $this->aspects = $aspects;
+  }
+  public function getAspects()
+  {
+    return $this->aspects;
   }
   public function setAvailability($availability)
   {
@@ -4900,6 +4910,41 @@ class Google_Service_ShoppingContent_Product extends Google_Collection
   public function getWarnings()
   {
     return $this->warnings;
+  }
+}
+
+class Google_Service_ShoppingContent_ProductAspect extends Google_Model
+{
+  protected $internal_gapi_mappings = array(
+  );
+  public $aspectName;
+  public $destinationName;
+  public $intention;
+
+
+  public function setAspectName($aspectName)
+  {
+    $this->aspectName = $aspectName;
+  }
+  public function getAspectName()
+  {
+    return $this->aspectName;
+  }
+  public function setDestinationName($destinationName)
+  {
+    $this->destinationName = $destinationName;
+  }
+  public function getDestinationName()
+  {
+    return $this->destinationName;
+  }
+  public function setIntention($intention)
+  {
+    $this->intention = $intention;
+  }
+  public function getIntention()
+  {
+    return $this->intention;
   }
 }
 
