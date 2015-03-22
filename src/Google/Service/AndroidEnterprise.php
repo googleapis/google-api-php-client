bbb2ee665e5ca0446192ea78eed58bb742453049
Silvano Luciani <silvano.luciani@gmail.com>
2015-03-22 05:14:38 -0700

    Updated AndroidEnterprise.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/AndroidEnterprise.php
+++ src/Google/Service/AndroidEnterprise.php
@@ -24,7 +24,7 @@
  *
  * <p>
  * For more information about this service, see the API
- * <a href="https://developers.google.com/play/enterprise" target="_blank">Documentation</a>
+ * <a href="" target="_blank">Documentation</a>
  * </p>
  *
  * @author Google, Inc.
