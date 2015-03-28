e190c5b2ed586eaef431d4d0e5f7d6e72c0deb71
Silvano Luciani <silvano.luciani@gmail.com>
2015-03-28 05:20:42 -0700

    Updated Drive.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Drive.php
+++ src/Google/Service/Drive.php
@@ -476,6 +476,14 @@ class Google_Service_Drive extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'projection' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
+                'revisionId' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'acknowledgeAbuse' => array(
                   'location' => 'query',
                   'type' => 'boolean',
@@ -483,10 +491,6 @@ class Google_Service_Drive extends Google_Service
                 'updateViewedDate' => array(
                   'location' => 'query',
                   'type' => 'boolean',
-                ),
-                'projection' => array(
-                  'location' => 'query',
-                  'type' => 'string',
                 ),
               ),
             ),'insert' => array(
@@ -706,6 +710,14 @@ class Google_Service_Drive extends Google_Service
                   'type' => 'string',
                   'required' => true,
                 ),
+                'projection' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
+                'revisionId' => array(
+                  'location' => 'query',
+                  'type' => 'string',
+                ),
                 'acknowledgeAbuse' => array(
                   'location' => 'query',
                   'type' => 'boolean',
@@ -713,10 +725,6 @@ class Google_Service_Drive extends Google_Service
                 'updateViewedDate' => array(
                   'location' => 'query',
                   'type' => 'boolean',
-                ),
-                'projection' => array(
-                  'location' => 'query',
-                  'type' => 'string',
                 ),
               ),
             ),
@@ -1710,12 +1718,15 @@ class Google_Service_Drive_Files_Resource extends Google_Service_Resource
    * @param string $fileId The ID for the file in question.
    * @param array $optParams Optional parameters.
    *
-   * @opt_param bool acknowledgeAbuse Whether the user is acknowledging the risk
-   * of downloading known malware or other abusive files.
-   * @opt_param bool updateViewedDate Whether to update the view date after
-   * successfully retrieving the file.
    * @opt_param string projection This parameter is deprecated and has no
    * function.
+   * @opt_param string revisionId Specifies the Revision ID that should be
+   * downloaded. Ignored unless alt=media is specified.
+   * @opt_param bool acknowledgeAbuse Whether the user is acknowledging the risk
+   * of downloading known malware or other abusive files. Ignored unless alt=media
+   * is specified.
+   * @opt_param bool updateViewedDate Whether to update the view date after
+   * successfully retrieving the file.
    * @return Google_Service_Drive_DriveFile
    */
   public function get($fileId, $optParams = array())
@@ -1903,12 +1914,15 @@ class Google_Service_Drive_Files_Resource extends Google_Service_Resource
    * @param Google_Channel $postBody
    * @param array $optParams Optional parameters.
    *
-   * @opt_param bool acknowledgeAbuse Whether the user is acknowledging the risk
-   * of downloading known malware or other abusive files.
-   * @opt_param bool updateViewedDate Whether to update the view date after
-   * successfully retrieving the file.
    * @opt_param string projection This parameter is deprecated and has no
    * function.
+   * @opt_param string revisionId Specifies the Revision ID that should be
+   * downloaded. Ignored unless alt=media is specified.
+   * @opt_param bool acknowledgeAbuse Whether the user is acknowledging the risk
+   * of downloading known malware or other abusive files. Ignored unless alt=media
+   * is specified.
+   * @opt_param bool updateViewedDate Whether to update the view date after
+   * successfully retrieving the file.
    * @return Google_Service_Drive_Channel
    */
   public function watch($fileId, Google_Service_Drive_Channel $postBody, $optParams = array())
