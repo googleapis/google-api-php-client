aaff0f4dde6cee26b8edc994aef34f8e1e34f15a
John Bacon <jpbacon@gmail.com>
2015-04-03 14:46:27 -0400

    Fix typo in comment
    "tigher" becomes "higher"

--- src/Google/IO/Curl.php
+++ src/Google/IO/Curl.php
@@ -148,7 +148,7 @@ class Google_IO_Curl extends Google_IO_Abstract
   {
     // Since this timeout is really for putting a bound on the time
     // we'll set them both to the same. If you need to specify a longer
-    // CURLOPT_TIMEOUT, or a tigher CONNECTTIMEOUT, the best thing to
+    // CURLOPT_TIMEOUT, or a higher CONNECTTIMEOUT, the best thing to
     // do is use the setOptions method for the values individually.
     $this->options[CURLOPT_CONNECTTIMEOUT] = $timeout;
     $this->options[CURLOPT_TIMEOUT] = $timeout;
