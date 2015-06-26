d9adb08535e3dfb8d9fd10eda939e3b1bf9f8449
Silvano Luciani <silvano.luciani@gmail.com>
2015-06-26 13:44:53 -0700

    Updated DoubleClickBidManager.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/DoubleClickBidManager.php
+++ src/Google/Service/DoubleClickBidManager.php
@@ -46,6 +46,7 @@ class Google_Service_DoubleClickBidManager extends Google_Service
   public function __construct(Google_Client $client)
   {
     parent::__construct($client);
+    $this->rootUrl = 'https://www.googleapis.com/';
     $this->servicePath = 'doubleclickbidmanager/v1/';
     $this->version = 'v1';
     $this->serviceName = 'doubleclickbidmanager';
