3a947490495540f4413dfd99fe1ba3e6dccd3275
Silvano Luciani <silvano.luciani@gmail.com>
2015-06-26 13:45:04 -0700

    Updated Tasks.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Tasks.php
+++ src/Google/Service/Tasks.php
@@ -49,6 +49,7 @@ class Google_Service_Tasks extends Google_Service
   public function __construct(Google_Client $client)
   {
     parent::__construct($client);
+    $this->rootUrl = 'https://www.googleapis.com/';
     $this->servicePath = 'tasks/v1/';
     $this->version = 'v1';
     $this->serviceName = 'tasks';
