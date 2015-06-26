d76bb34b6c9d64d5836e8e25af3661fff3ff7022
Silvano Luciani <silvano.luciani@gmail.com>
2015-06-26 13:45:15 -0700

    Updated Fusiontables.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Fusiontables.php
+++ src/Google/Service/Fusiontables.php
@@ -53,6 +53,7 @@ class Google_Service_Fusiontables extends Google_Service
   public function __construct(Google_Client $client)
   {
     parent::__construct($client);
+    $this->rootUrl = 'https://www.googleapis.com/';
     $this->servicePath = 'fusiontables/v2/';
     $this->version = 'v2';
     $this->serviceName = 'fusiontables';
