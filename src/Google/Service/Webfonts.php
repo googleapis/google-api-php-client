2513304c631677093c2ed5e497a71bc3aa50bad9
Silvano Luciani <silvano.luciani@gmail.com>
2015-06-26 13:44:48 -0700

    Updated Webfonts.php
    This change has been generated by a script that has detected changes in the
    discovery doc of the API.
    
    Check http://developers.google.com/discovery for more info.

--- src/Google/Service/Webfonts.php
+++ src/Google/Service/Webfonts.php
@@ -43,6 +43,7 @@ class Google_Service_Webfonts extends Google_Service
   public function __construct(Google_Client $client)
   {
     parent::__construct($client);
+    $this->rootUrl = 'https://www.googleapis.com/';
     $this->servicePath = 'webfonts/v1/';
     $this->version = 'v1';
     $this->serviceName = 'webfonts';
