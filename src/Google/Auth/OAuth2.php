518279c8826120e049de42aaa7f76eadebeac8ee
InomataMitsuhiro <inomata@tech-vein.com>
2015-10-14 18:55:21 +0900

    fix travis ci code rules.

--- src/Google/Auth/OAuth2.php
+++ src/Google/Auth/OAuth2.php
@@ -489,8 +489,12 @@ class Google_Auth_OAuth2 extends Google_Auth_Abstract
       $audience = $this->client->getClassConfig($this, 'client_id');
     }
 
-    return $this->verifySignedJwtWithCerts($id_token, $certs, $audience,
-      array(self::OAUTH2_ISSUER, self::HTTPS_OAUTH2_ISSUER));
+    return $this->verifySignedJwtWithCerts(
+        $id_token,
+        $certs,
+        $audience,
+        array(self::OAUTH2_ISSUER, self::HTTPS_OAUTH2_ISSUER)
+    );
   }
 
   /**
