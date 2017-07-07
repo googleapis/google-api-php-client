b372b5aac01df2d356ef491e88d8801cd86c42ac
Claire Ryan <sypherlev@users.noreply.github.com>
2017-07-07 11:25:27 -0700

    Removed both checks
    Removed the is_array check and the isset($paramSpec['repeated']) check.

--- src/Google/Service/Resource.php
+++ src/Google/Service/Resource.php
@@ -272,14 +272,11 @@ class Google_Service_Resource
       if ($paramSpec['location'] == 'path') {
         $uriTemplateVars[$paramName] = $paramSpec['value'];
       } else if ($paramSpec['location'] == 'query') {
-        if (isset($paramSpec['repeated']) && is_array($paramSpec['value'])) {
+        if (is_array($paramSpec['value'])) {
           foreach ($paramSpec['value'] as $value) {
             $queryVars[] = $paramName . '=' . rawurlencode(rawurldecode($value));
           }
         } else {
-          if (is_array($paramSpec['value'])) {
-            $paramSpec['value'] = array_shift($paramSpec['value']);
-          }
           $queryVars[] = $paramName . '=' . rawurlencode(rawurldecode($paramSpec['value']));
         }
       }
