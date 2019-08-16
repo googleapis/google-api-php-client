7ea102e2a17a0d173929097f3ebb4663067ba571
John Pedrie <jdpedrie@users.noreply.github.com>
2019-08-16 15:16:22 -0400

    docs: Add note about cached access token (#1696)
    * docs: Add note about cached access token
    
    * warn about clear cache

--- src/Google/Client.php
+++ src/Google/Client.php
@@ -416,6 +416,17 @@ class Google_Client
   }
 
   /**
+   * Set the access token used for requests.
+   *
+   * Note that at the time requests are sent, tokens are cached. A token will be
+   * cached for each combination of service and authentication scopes. If a
+   * cache pool is not provided, creating a new instance of the client will
+   * allow modification of access tokens. If a persistent cache pool is
+   * provided, in order to change the access token, you must clear the cached
+   * token by calling `$client->getCache()->clear()`. (Use caution in this case,
+   * as calling `clear()` will remove all cache items, including any items not
+   * related to Google API PHP Client.)
+   *
    * @param string|array $token
    * @throws InvalidArgumentException
    */
