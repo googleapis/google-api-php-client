19d7d735ee4cff0f8c14a234b5094b99d00ef278
Jakub Paś <jakubpas@users.noreply.github.com>
2020-07-22 21:51:49 +0200

    chore: fix deprecated syntax (#1875)

--- src/Google/Http/REST.php
+++ src/Google/Http/REST.php
@@ -170,7 +170,7 @@ class Google_Http_REST
     }
 
     if (count($queryVars)) {
-      $requestUrl .= '?' . implode($queryVars, '&');
+      $requestUrl .= '?' . implode('&', $queryVars);
     }
 
     return $requestUrl;
--- src/Google/Utils.php
+++ src/Google/Utils.php
@@ -62,7 +62,7 @@ class Google_Utils
     $strlenVar = strlen($str);
     $d = $ret = 0;
     for ($count = 0; $count < $strlenVar; ++ $count) {
-      $ordinalValue = ord($str{$ret});
+      $ordinalValue = ord($str[$ret]);
       switch (true) {
         case (($ordinalValue >= 0x20) && ($ordinalValue <= 0x7F)):
           // characters U-00000000 - U-0000007F (same as ASCII)
