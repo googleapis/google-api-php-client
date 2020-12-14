47c425042d989c9d04b4f88a26b405c845253ce2
Niels <niels@spatie.be>
2020-12-14 19:53:11 +0100

    update script

--- .github/apply-phpunit-patches.sh
+++ .github/apply-phpunit-patches.sh
@@ -6,11 +6,9 @@
 set -e
 
 # Be in the root dir
-cd "$(dirname $0)/../"
+cd "$(dirname "$0")/../"
 
-find tests/ -type f -print0 | xargs -0 sed -i 's/function setUpBeforeClass(): void/function setUpBeforeClass()/g';
 find tests/ -type f -print0 | xargs -0 sed -i 's/function setUp(): void/function setUp()/g';
-find tests/ -type f -print0 | xargs -0 sed -i 's/function tearDown(): void/function tearDown()/g';
 
 # Drop the listener from the config file
 sed -i '/<listeners>/,+2d' phpunit.xml.dist;
