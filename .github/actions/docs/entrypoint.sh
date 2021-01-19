7a09507beff1048cc9a29417f78a98284f06214f
John Pedrie <john@pedrie.com>
2021-01-19 12:31:44 -0500

    chore: fix release and documentation actions (#2030)

--- .github/actions/docs/entrypoint.sh
+++ .github/actions/docs/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -l
 
 apt-get update
-apt-get install -y git
+apt-get install -y git wget
 git reset --hard HEAD
 
 # Create the directories
 mkdir .docs
 mkdir .cache
 
+wget https://github.com/jdpedrie/Sami/releases/download/v4.3.0/sami.phar
+
 # Run the docs generation command
-php vendor/bin/sami.php update .github/actions/docs/sami.php
+php sami.phar update .github/actions/docs/sami.php
+chmod -R 0777 .
