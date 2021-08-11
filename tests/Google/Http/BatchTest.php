3efab7d71373e1483d37b266a37c887c7784bca8
Pascal Birchler <pascalb@google.com>
2021-08-11 10:33:51 +0200

    Don't use deprecated assertion methods

--- tests/Google/Http/BatchTest.php
+++ tests/Google/Http/BatchTest.php
@@ -86,8 +86,8 @@ class BatchTest extends BaseTest
     /** @var \GuzzleHttp\Psr7\Request $request */
     $request = $storage->objects->insert($bucket, $obj, $params);
 
-    $this->assertContains('multipart/related', $request->getHeaderLine('content-type'));
-    $this->assertContains('/upload/', $request->getUri()->getPath());
-    $this->assertContains('uploadType=multipart', $request->getUri()->getQuery());
+    $this->assertStringContainsString('multipart/related', $request->getHeaderLine('content-type'));
+    $this->assertStringContainsString('/upload/', $request->getUri()->getPath());
+    $this->assertStringContainsString('uploadType=multipart', $request->getUri()->getQuery());
   }
 }
